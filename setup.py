74c23f9af87786cde7455813dcd3d7c497682135
Nathaniel Wong <natmikewong@gmail.com>
2018-06-02 18:59:16 -0500

    pip safe

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sgbridgebot',
+      version='0.0.6-alpha',
+      description='A Telegram bot that allows users to play floating bridge',
+      url='http://github.com/zuzivian/sgbridgebot',
+      author='Nathaniel Wong',
+      author_email='rubikcode@gmail.com',
+      license='MIT',
+      packages=['sgbridgebot'],
+      zip_safe=False)
