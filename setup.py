f4eb41dba0be02e3ee7b5051850ecb068840dc36
Nathaniel Wong <NathanielWongKinYew2019@u.northwestern.edu>
2026-02-08 18:10:16 +0800

    Add runtime dependency to package metadata

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(name='sgbridgebot',
       author_email='rubikcode@gmail.com',
       license='MIT',
       packages=find_packages(include=['sgbridgebot', 'sgbridgebot.*']),
+      install_requires=['python-telegram-bot[webhooks]>=21,<22'],
       extras_require={'dev': ['pytest>=8,<9']},
       zip_safe=False)
